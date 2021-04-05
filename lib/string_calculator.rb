ff167e8bbd7973868f97af8e023d9665756adabd
Dan Pappo <dpappo@gmail.com>
2021-04-05 09:38:34 -0400

    refactor for multiple use cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,7 +4,8 @@ class StringCalculator
     if input.empty?
       0
     else
-      input.to_i
+      numbers = input.split(",").map { |num| num.to_i }
+      numbers.inject(0) { |sum, number| sum + number }
     end
   end
 
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -20,5 +20,24 @@ describe StringCalculator do
         expect(StringCalculator.add('10')).to eq(10)
       end
     end
+
+    context "two numbers" do
+      context "given '2, 4'" do
+        it 'returns 6' do
+          expect(StringCalculator.add('2,4')).to eq(6)
+        end
+      end
+    end
+
+    context "two numbers" do
+      context "given '17, 100'" do
+        it 'returns 117' do
+          expect(StringCalculator.add('17,100')).to eq(117)
+        end
+      end
+    end
+
+
+
   end
 end
